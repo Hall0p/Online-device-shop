71594af35b82b39441dbb26c37b17371b2ce41b0
Rostislav Nazarov <nazarov_rostislav@mail.ru>
2022-06-13 21:34:30 +0300

    Edit: made brands expandable
    Relocated brands into main wrapper and deleted sidebar margins

--- src/components/Devices.jsx
+++ src/components/Devices.jsx
@@ -2,41 +2,45 @@ import React from 'react';
 import {Link, useSearchParams} from "react-router-dom";
 import star from "../assets/img/star.svg";
 import {useSelector} from "react-redux";
+import Brands from "./Brands";
 
 const Devices = () => {
   const [searchParams, setSearchParams] = useSearchParams()
 
   const devices = useSelector(state => state.devices)
   return (
-    <div className="devices">
-      {devices.filter( device => {
-        let category = searchParams.get('category')
-        let brand = searchParams.get('brand')
-        if (!category && !brand) return true
-        return ((category ? category === device.category : true) && (brand ? brand === device.brand : true))
-      })
-        .map(device =>
-          <div key={device.id} className='device'>
-            <Link to={'/device/' + device.id} className="device">
-            <img src={device.image} alt="device" className="device__img"/>
-            <div className="device-info">
-              <div className='device-info__wrapper'>
-                <span className="device__type">
-                  {device.brand}
-                </span>
-                <div className="device-rating">
-                  <span className="device-rating__rating">
-                    {device.rating}
+    <div className="main-wrapper">
+      <Brands/>
+      <div className="devices">
+        {devices.filter(device => {
+          let category = searchParams.get('category')
+          let brand = searchParams.get('brand')
+          if (!category && !brand) return true
+          return ((category ? category === device.category : true) && (brand ? brand === device.brand : true))
+        })
+          .map(device =>
+            <div key={device.id} className='device'>
+              <Link to={'/device/' + device.id} className="device">
+                <img src={device.image} alt="device" className="device__img"/>
+                <div className="device-info">
+                  <div className='device-info__wrapper'>
+                    <span className="device__type">
+                      {device.brand}
+                    </span>
+                    <div className="device-rating">
+                      <span className="device-rating__rating">
+                        {device.rating}
+                      </span>
+                      <img src={star} alt="star" className="device-rating__star"/>
+                    </div>
+                  </div>
+                  <span className="device__name">
+                    {device.name}
                   </span>
-                  <img src={star} alt="star" className="device-rating__star"/>
                 </div>
-              </div>
-              <span className="device__name">
-                {device.name}
-              </span>
-            </div>
-          </Link></div>
-        )}
+              </Link></div>
+          )}
+      </div>
     </div>
   );
 };
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Sidebar from "../components/Sidebar";
-import Brands from "../components/Brands";
 import Devices from "../components/Devices";
 
 const Main = () => {
@@ -9,7 +8,6 @@ const Main = () => {
       <main className="main">
         <Sidebar />
         <div className="content">
-          <Brands />
           <Devices />
         </div>
       </main>
